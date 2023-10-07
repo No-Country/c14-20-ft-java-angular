ea63fb28e43707f30bdce3b17fb5349a6ec5f816
franorther <franorther@gmail.com>
2023-10-06 23:12:21 -0500

    initial

---
+++ backend/src/main/java/com/no_country/foodTech_delivery/FoodTechDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.no_country.foodTech_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodTechDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodTechDeliveryApplication.class, args);
+	}
+
+}
