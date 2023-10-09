b5d8fe511f3ddb624efaaf41e3bb6e34298acab9
brunoosuna8 <brunoosuna8@gmail.com>
2023-10-09 15:44:36 -0300

    add:product.service,single-product component

--- develop_front/src/app/app.module.ts
+++ develop_front/src/app/app.module.ts
@@ -3,19 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './components/login/login.component';
 import { MessageComponent } from './components/message/message.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './modules/shared/shared.module';
+import { SingleProductComponent } from './components/single-product/single-product.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, MessageComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    MessageComponent,
+    SingleProductComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- develop_front/src/app/services/product/product.service.ts
+++ develop_front/src/app/services/product/product.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Product } from 'src/app/interfaces/Product.interface';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-
-  constructor() { }
+  arrayProducts: Product[] = [];
+  arrayObs: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>([]);
+  constructor(private http: HttpClient) {}
+  get products() {
+    return this.http.get<Product[]>('../../../assets/data/platos.json');
+  }
 }
