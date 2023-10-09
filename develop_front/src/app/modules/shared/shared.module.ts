19ea75c30d885ec12d51caad71d46943ec8ec61b
Fair Lucas <lukitaa.27@gmail.com>
2023-10-09 18:29:06 -0300

    Login

--- develop_front/src/app/modules/shared/shared.module.ts
+++ develop_front/src/app/modules/shared/shared.module.ts
@@ -5,10 +5,17 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatIconModule, MatButtonModule, MatToolbarModule],
-  exports: [CommonModule, MatIconModule, MatButtonModule, MatToolbarModule],
+  imports: [CommonModule, MatIconModule, MatButtonModule, MatToolbarModule, MatNativeDateModule, FormsModule, ReactiveFormsModule,
+    MatCardModule, MatFormFieldModule],
+  exports: [CommonModule, MatIconModule, MatButtonModule, MatToolbarModule, MatNativeDateModule, FormsModule, ReactiveFormsModule,
+    MatCardModule, MatFormFieldModule],
 })
 export class SharedModule {}
