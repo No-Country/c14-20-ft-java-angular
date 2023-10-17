a6a64ef05a7bfa8acf62549d734f9cbbe74415d0
brunoosuna8 <brunoosuna8@gmail.com>
2023-10-17 16:25:01 -0300

    fix:header count now show quantity of products even if they are repeated

--- develop_front/src/app/components/header/header.component.ts
+++ develop_front/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartProduct } from 'src/app/interfaces/CartProduct.interface';
 import { User } from 'src/app/interfaces/User.interface';
 import { CredentialsService } from 'src/app/services/credentials.service';
 import { ShoppingCartService } from 'src/app/services/shopping-cart/shopping-cart.service';
@@ -10,14 +11,17 @@ import { ShoppingCartService } from 'src/app/services/shopping-cart/shopping-car
 })
 export class HeaderComponent implements OnInit {
   userData!: User;
-  count!: number;
+  count: number = 0; //si se trae los datos del back aca la cantidad  deberia iniciarlizarse con los datos de lback, no en 0
   constructor(
     private shoppingCartService: ShoppingCartService,
     private credentials: CredentialsService
   ) {}
   ngOnInit() {
     this.shoppingCartService.productsInCart.subscribe((data) => {
-      this.count = data.length;
+      this.count = data.reduce((acc: number, e: CartProduct) => {
+        acc += e.quantity;
+        return acc;
+      }, 0);
       console.log(this.count);
       this.credentials.userData.subscribe((data) => (this.userData = data));
       console.log(this.userData);
