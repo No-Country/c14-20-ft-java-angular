77098c8121507443319fe6f83a7416c76882a129
Torneyus <jjd21017@gmail.com>
2023-11-01 16:35:38 -0500

    traer la info de un producto sin logearte

--- backend/src/main/java/com/no_country/foodTech_delivery/api/security/SecurityConfig.java
+++ backend/src/main/java/com/no_country/foodTech_delivery/api/security/SecurityConfig.java
@@ -53,6 +53,7 @@ public class SecurityConfig {
                     auth.requestMatchers(HttpMethod.POST, "/login").permitAll();
                     auth.requestMatchers(HttpMethod.POST, "/api/users").permitAll();
                     auth.requestMatchers(HttpMethod.GET, "api/products").permitAll();
+                    auth.requestMatchers(HttpMethod.GET, "api/products/**").permitAll();
                     auth.anyRequest().authenticated();
                 })
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
